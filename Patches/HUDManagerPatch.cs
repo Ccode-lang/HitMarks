36e28ee4eba686efde9c66ec8bde3a4ad3dfce2d
Ccode-lang <coop112097@gmail.com>
2024-02-07 19:39:52 -0700

    Add project files.

---
+++ Patches/HUDManagerPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HitMarks.Patches
+{
+    [HarmonyPatch(typeof(EnemyAI), "HitEnemyOnLocalClient")]
+    internal class EnemyAIPatch
+    {
+        public static void Postfix(EnemyAI __instance)
+        {
+            GameObject gm = UnityEngine.Object.Instantiate(Plugin.newUI);
+            __instance.StartCoroutine(Utils.DeleteAfter1Sec(gm, __instance));
+        }
+    }
+}
+
+namespace HitMarks
+{
+    public class Utils
+    {
+        public static IEnumerator DeleteAfter1Sec(GameObject gm, MonoBehaviour mono)
+        {
+            yield return new WaitForSeconds(1);
+            UnityEngine.Object.Destroy(gm);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HitMarks
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.HitMarks";
+        private const string PluginName = "HitMarks";
+        private const string VersionString = "1.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        public static GameObject newUI;
+        public void Awake()
+        {
+            string location = ((BaseUnityPlugin)this).Info.Location;
+            string text = "HitMarks.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string text3 = text2 + "hitmarker";
+            AssetBundle val = AssetBundle.LoadFromFile(text3);
+            newUI = val.LoadAsset<GameObject>("Hitmarker Container");
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
